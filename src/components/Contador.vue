2d589d7e23dee420360c3ea77213c748f6f96e36
DAscencioSanchez <diaspm77@gmail.com>
2022-03-24 11:10:33 -0600

    source commit

---
+++ src/components/Contador.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <div class="contador">
+        {{ $store.state.contador }}
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: 'Contador',
+}
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Contador/>
+    <contadorCuadrado/>
+    <botones/>
+  </div>
+</template>
+
+<script>
+
+import Contador from "../components/Contador.vue"
+import contadorCuadrado from "../components/contadorCuadrado.vue"
+import Botones from "../components/Botones.vue"
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    Contador,
+    contadorCuadrado,
+    Botones,
+  }
+}
+
+</script>
+
+<style>
+  div{
+    margin-bottom:10px;
+  }
+  .contador{
+    font-size:80px;
+  }
+  .botones button {
+    font-size: 40px;
+    width:100px;
+    margin:20px;
+  }
+  .cuadrado{
+    font-size:25px;
+  }
+
+</style>
